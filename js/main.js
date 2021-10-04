5c9adb104222842f54390eaa4b308d85332cba63
Troy Dorman <troydorman@gmail.com>
2021-10-03 20:24:51 -0600

    created some event listeners

--- js/main.js
+++ js/main.js
@@ -2,4 +2,7 @@ const modal = document.querySelector("#my-modal");
 const modalBtn = document.querySelector("#modal-btn");
 const closeBtn = document.querySelector("close");
 
-
+// events
+modalBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+window.addEventListener("click", outsideClick);
