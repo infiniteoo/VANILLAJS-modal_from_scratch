7c7845b68188ad9aa14f0e43013304d888c64e54
Troy Dorman <troydorman@gmail.com>
2021-10-03 20:27:21 -0600

    created outsideClick function

--- js/main.js
+++ js/main.js
@@ -9,6 +9,15 @@ window.addEventListener("click", outsideClick);
 
 // open
 function openModal() {
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
 
+function closeModal() {
+  modal.style.display = "none";
+}
+
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+}
