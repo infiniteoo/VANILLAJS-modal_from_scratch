3b99afc428e3d56247ea47c8d5e0db4b9d959f47
Troy Dorman <troydorman@gmail.com>
2021-10-03 20:31:28 -0600

    styled modal footer.  fixed typo in close button reference var

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const modal = document.querySelector("#my-modal");
 const modalBtn = document.querySelector("#modal-btn");
-const closeBtn = document.querySelector("close");
+const closeBtn = document.querySelector(".close");
 
 // events
 modalBtn.addEventListener("click", openModal);
